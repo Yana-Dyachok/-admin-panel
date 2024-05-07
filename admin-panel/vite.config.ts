8a381cacb2f6441881ceeb316532824c0b50ac4c
Yana-Dyachok <annetart1994@gmail.com>
2024-05-07 12:01:27 +0300

    feat: add deploy settings

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/admin-panel/admin-panel"
+  base: "/admin-panel/"
 })
